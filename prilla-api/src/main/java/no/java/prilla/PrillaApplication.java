d1acdf91278f3509ea3b8b0c364bd158abccdc17
Børge Nese <Borge.Nese@ks.no>
2019-01-19 14:51:37 +0100

    java init

---
+++ prilla-api/src/main/java/no/java/prilla/PrillaApplication.java
@@ -0,0 +1,14 @@
+package no.java.prilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrillaApplication.class, args);
+	}
+
+}
+
